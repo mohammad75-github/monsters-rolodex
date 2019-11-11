c519253cc9fb7dd7940bd4dd2e9b0dbded9ef8e5
mohammad75-github <pourbarfehm@gmail.com>
2019-11-11 17:21:06 +0330

    Updates

---
+++ precache-manifest.dffb803ea5ffb9aec91758f6010b43c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93f56b116b74ba43a6b2e671c62f5e1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "95183dcc7a90759e1d3d",
+    "url": "/monsters-rolodex/static/css/main.6cd27ce9.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "95183dcc7a90759e1d3d",
+    "url": "/monsters-rolodex/static/js/main.24e523d0.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
